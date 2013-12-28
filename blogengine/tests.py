5ff3a21a9fe49b00e29af292d29d85e2e377fc1a
Matthew Daly <matthewbdaly@gmail.com>
2013-12-28 20:58:16 +0000

    Refactored admin test

--- blogengine/tests.py
+++ blogengine/tests.py
@@ -35,12 +35,12 @@ class PostTest(TestCase):
 class AdminTest(LiveServerTestCase):
     fixtures = ['users.json']
 
-    def test_login(self):
-        # Create client
-        c = Client()
+    def setUp(self):
+        self.client = Client()
 
+    def test_login(self):
         # Get login page
-        response = c.get('/admin/')
+        response = self.client.get('/admin/')
 
         # Check response code
         self.assertEquals(response.status_code, 200)
@@ -49,34 +49,31 @@ class AdminTest(LiveServerTestCase):
         self.assertTrue('Log in' in response.content)
 
         # Log the user in
-        c.login(username='bobsmith', password="password")
+        self.client.login(username='bobsmith', password="password")
 
         # Check response code
-        response = c.get('/admin/')
+        response = self.client.get('/admin/')
         self.assertEquals(response.status_code, 200)
 
         # Check 'Log out' in response
         self.assertTrue('Log out' in response.content)
 
     def test_logout(self):
-        # Create client
-        c = Client()
-
         # Log in
-        c.login(username='bobsmith', password="password")
+        self.client.login(username='bobsmith', password="password")
 
         # Check response code
-        response = c.get('/admin/')
+        response = self.client.get('/admin/')
         self.assertEquals(response.status_code, 200)
 
         # Check 'Log out' in response
         self.assertTrue('Log out' in response.content)
 
         # Log out
-        c.logout()
+        self.client.logout()
 
         # Check response code
-        response = c.get('/admin/')
+        response = self.client.get('/admin/')
         self.assertEquals(response.status_code, 200)
 
         # Check 'Log in' in response
