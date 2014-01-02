c717ba4c6f1793507b8c076f5d7f54a7b5a4d89a
Matthew Daly <matthewbdaly@gmail.com>
2014-01-02 15:40:17 +0000

    Added Markdown support

--- blogengine/tests.py
+++ blogengine/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, LiveServerTestCase, Client
 from django.utils import timezone
 from blogengine.models import Post
+import markdown
 
 # Create your tests here.
 class PostTest(TestCase):
@@ -173,7 +174,7 @@ class PostViewTest(LiveServerTestCase):
         # Create the post
         post = Post()
         post.title = 'My first post'
-        post.text = 'This is my first blog post'
+        post.text = 'This is [my first blog post](http://127.0.0.1:8000/)'
         post.pub_date = timezone.now()
         post.save()
 
@@ -189,9 +190,12 @@ class PostViewTest(LiveServerTestCase):
         self.assertTrue(post.title in response.content)
 
         # Check the post text is in the response
-        self.assertTrue(post.text in response.content)
+        self.assertTrue(markdown.markdown(post.text) in response.content)
 
         # Check the post date is in the response
         self.assertTrue(str(post.pub_date.year) in response.content)
         self.assertTrue(post.pub_date.strftime('%b') in response.content)
         self.assertTrue(str(post.pub_date.day) in response.content)
+
+        # Check the link is marked up properly
+        self.assertTrue('<a href="http://127.0.0.1:8000/">my first blog post</a>' in response.content)
