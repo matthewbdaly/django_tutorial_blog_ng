a812c9a061949a6acce33844a8311f5cd7bc4216
Matthew Daly <matthewbdaly@gmail.com>
2014-02-26 20:53:47 +0000

    Amended settings to work on desktop and Heroku

--- django_tutorial_blog_ng/settings.py
+++ django_tutorial_blog_ng/settings.py
@@ -64,8 +64,8 @@ WSGI_APPLICATION = 'django_tutorial_blog_ng.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.',
+        'NAME': '',
     }
 }
 
@@ -97,3 +97,21 @@ JENKINS_TASKS = (
     'django_jenkins.tasks.with_coverage',
 )
 PROJECT_APPS = ['blogengine']
+
+# Heroku config
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config(default="sqlite:///db.sqlite3")
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
