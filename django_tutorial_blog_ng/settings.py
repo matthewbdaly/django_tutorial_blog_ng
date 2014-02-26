fa0745d284a07ade8dded2840f5254b5915237b0
Matthew Daly <matthewbdaly@gmail.com>
2014-02-26 19:59:42 +0000

    Added coverage checking using django-jenkins

--- django_tutorial_blog_ng/settings.py
+++ django_tutorial_blog_ng/settings.py
@@ -90,3 +90,10 @@ STATIC_URL = '/static/'
 
 # Template directory
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+INSTALLED_APPS += ('django_jenkins',)
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.with_coverage',
+)
+PROJECT_APPS = ['blogengine']
