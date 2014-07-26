9ddf4b0550b0c005dd0d9cf8769f964b6ebd0630
Matthew Daly <matthewbdaly@gmail.com>
2014-07-26 21:49:44 +0100

    Test in place for search

--- blogengine/tests.py
+++ blogengine/tests.py
@@ -804,3 +804,32 @@ class FlatPageViewTest(BaseAcceptanceTest):
         # Check title and content in response
         self.assertTrue('About me' in response.content)
         self.assertTrue('All about me' in response.content)
+
+
+class SearchViewTest(BaseAcceptanceTest):
+    def test_search(self):
+        # Create a post
+        post = PostFactory()
+
+        # Create another post
+        post2 = PostFactory(text='This is my *second* blog post', title='My second post', slug='my-second-post')
+
+        # Search for first post
+        response = self.client.get('/search?q=first')
+        self.assertEquals(response.status_code, 200)
+
+        # Check the first post is contained in the results
+        self.assertTrue('My first post' in response.content)
+
+        # Check the second post is not contained in the results
+        self.assertTrue('My second post' not in response.content)
+
+        # Search for second post
+        response = self.client.get('/search?q=second')
+        self.assertEquals(response.status_code, 200)
+
+        # Check the first post is not contained in the results
+        self.assertTrue('My first post' not in response.content)
+
+        # Check the second post is contained in the results
+        self.assertTrue('My second post' in response.content)
