f02009a3eb9137c2036c88fdcf34d7b2822b6464
Matthew Daly <matthewbdaly@gmail.com>
2014-09-28 21:58:52 +0100

    Upgraded to Django 1.7

--- blogengine/migrations/0001_initial.py
+++ blogengine/migrations/0001_initial.py
@@ -1,20 +1,64 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        pass
+    dependencies = [
+        ('sites', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-    def backwards(self, orm):
-        pass
-
-    models = {
-        
-    }
-
-    complete_apps = ['blogengine']
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('description', models.TextField()),
+                ('slug', models.SlugField(max_length=40, unique=True, null=True, blank=True)),
+            ],
+            options={
+                'verbose_name_plural': 'categories',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('pub_date', models.DateTimeField()),
+                ('text', models.TextField()),
+                ('slug', models.SlugField(unique=True, max_length=40)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('category', models.ForeignKey(blank=True, to='blogengine.Category', null=True)),
+                ('site', models.ForeignKey(to='sites.Site')),
+            ],
+            options={
+                'ordering': ['-pub_date'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('description', models.TextField()),
+                ('slug', models.SlugField(max_length=40, unique=True, null=True, blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='tags',
+            field=models.ManyToManyField(to='blogengine.Tag', null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
--- django_tutorial_blog_ng/settings.py
+++ django_tutorial_blog_ng/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south',
     'blogengine',
     'django.contrib.sites',
     'django.contrib.flatpages',
