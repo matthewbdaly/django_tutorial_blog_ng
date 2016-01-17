3d5662ee6ac1bde9f84b0f5122b57bd635d5c31b
Matthew Daly <matthewbdaly@gmail.com>
2016-01-17 17:04:08 +0000

    Switched off debug mode

--- django_tutorial_blog_ng/settings.py
+++ django_tutorial_blog_ng/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'fds-nk_-a1mj&n^nghc$dp@r0*fvcp1gywnenapindvifdh4f5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
